b2c7ea8885007ffec6393945ab9373d8fb94ce46
Andrey Grach <andrejgrach@icloud.com>
2021-01-27 18:02:24 +0300

    What should I do with interpolation?

--- src/Home/BeautifulPlacesCards/BeautifulPlacesCards.tsx
+++ src/Home/BeautifulPlacesCards/BeautifulPlacesCards.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 interface BeautifulPlacesCardsProps {
@@ -8,8 +8,25 @@ import {HomeNavigationProps} from "../../components/Navigation";
 import Background from "./Background";
 import Card from "./Card";
 import {Box, Header} from "../../components";
-
+import {useTransition} from "react-native-redash/lib/module/v1";
+import {interpolate} from "react-native-reanimated";
+const cards = [
+    {
+        index: 3,
+    },
+    {
+        index: 2,
+    },
+    {
+        index: 1,
+    },
+    {
+        index: 0,
+    }
+]
 const BeautifulPlacesCards = ({ navigation }: HomeNavigationProps<"BeautifulPlacesCards">) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const aIndex = useTransition(currentIndex)
     return (
         <Box flex={1} backgroundColor="white">
             <Header
@@ -19,9 +36,19 @@ const BeautifulPlacesCards = ({ navigation }: HomeNavigationProps<"BeautifulPlac
             />
             <Box flex={1}>
                 <Background />
-                <Card position={1} />
-                <Card position={0.5} />
-                <Card position={0} />
+                {cards.map(
+                    ({index}) =>
+                        index >= currentIndex && (
+                            <Card
+                                key={index}
+                                position={interpolate(index, {
+                                    inputRange: [0, 0.5, 1],
+                                    outputRange: [0, 1],
+                                })}
+                            />
+                    ))
+                }
+
             </Box>
         </Box>
     )
--- src/Home/BeautifulPlacesCards/Card.tsx
+++ src/Home/BeautifulPlacesCards/Card.tsx
@@ -1,28 +1,62 @@
 import React from "react";
 import {StyleSheet, Dimensions} from "react-native";
-import Animated, {interpolate} from "react-native-reanimated";
+import Animated, {add} from "react-native-reanimated";
 import {Box} from "../../components";
-import {interpolateColor} from "react-native-redash/lib/typescript/v1";
-
+import {mix} from "react-native-redash";
+import {PanGestureHandler} from "react-native-gesture-handler";
+import {usePanGestureHandler, withSpring} from "react-native-redash/lib/module/v1";
+import {useTransition} from "react-native-redash/lib/typescript/v1";
 
 
 const {width: wWidth} = Dimensions.get("window");
-const width = wWidth * 0.8;
-const height = width * (294 / 425);
+const width = wWidth * 0.7;
+const height = width * (425 / 294);
+const borderRadius = 24;
+
 interface CardProps {
     position: Animated.Adaptable<number>;
 }
-const Card = ({ position }: CardProps) => {
-    const backgroundColor = interpolate(position, {inputRange: [0, 1],
-        outputRange:["#C9E9E7" , "#74BCB8"]
-    });
+
+const Card = ({position}: CardProps) => {
+    const {gestureHandler, translation, velocity, state} = usePanGestureHandler()
+    const backgroundColor = Animated.interpolateColors(position, {
+        inputRange: [0, 1],
+        outputColorRange: ['#C9E9E7', '#74BCB8']
+    })
+    const translateYOffset = mix(position, 0, -50)
+    const scale = mix(position, 1, 0.9);
+    const translateX = withSpring({
+        value: translation.x,
+        velocity: velocity.x,
+        state,
+        snapPoints: [-width, 0, width]
+    })
+    const translateY = add(
+        translateYOffset,
+        withSpring({
+            value: translation.y,
+            velocity: velocity.y,
+            state,
+            snapPoints: [0]
+        })
+    )
     return (
         <Box
             style={StyleSheet.absoluteFill}
             justifyContent="center"
             alignItems="center"
         >
-            <Animated.View style={{ backgroundColor, width, height }}/>
+            <PanGestureHandler {...gestureHandler}>
+                <Animated.View
+                    style={{
+                        backgroundColor,
+                        width,
+                        height,
+                        borderRadius,
+                        transform: [{translateY}, {translateX}, {scale}]
+                    }}
+                />
+            </PanGestureHandler>
         </Box>
     )
 };
